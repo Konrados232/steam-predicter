6e1f283874a3a33063f553f46be130a462581638
Konrad Nowak <shaderpl@gmail.com>
2022-06-07 22:33:42 +0200

    Add converting fetched data to dictionary

--- Cleaner.py
+++ Cleaner.py
@@ -1,8 +1,10 @@
 
 
 class Cleaner():
-    def __init__(self, categories_list):
+    def __init__(self, base_dict, categories_list, tags_list):
+        self.base_dict = base_dict
         self.categories_list = categories_list
+        self.tags_list = tags_list
 
 
     def _extract_data(self, data, appid):
@@ -17,8 +19,12 @@ class Cleaner():
         return int(game_data["required_age"])
 
 
-    def _get_description(self, game_data):
+    def _get_short_description(self, game_data):
         return game_data["short_description"]
+    
+    
+    def _get_detailed_description(self, game_data):
+        return game_data["detailed_description"]
 
 
     def _is_english(self, game_data):
@@ -63,47 +69,35 @@ class Cleaner():
         return game_data["publishers"][0]
 
 
+    def _assign_categories(self, cat_data, new_dict):
+        for i in self.categories_list:
+            if i in cat_data:
+                new_dict[i] = 1
+
+
+    def _assign_tags(self, tags_data, new_dict):
+        for i in self.tags_list:
+            if i in tags_data:
+                new_dict[i] = 1
+
 
     def clean_data(self, data, appid):
+        new_dict = dict(self.base_dict)
         game_data = self._extract_data(data, appid)
 
-        name = self._get_name(game_data)
-        print(name)
+        new_dict["name"]  = self._get_name(game_data)
+        new_dict["required_age"]  = self._get_required_age(game_data)
+        new_dict["desc"] = self._get_short_description(game_data)
+        new_dict["english"] = self._is_english(game_data)
+        new_dict["windows"] = self._is_windows(game_data)
+        new_dict["mac"] = self._is_mac(game_data)
+        new_dict["linux"] = self._is_linux(game_data)
+        new_dict["achievements"] = self._get_achievements(game_data)
+        new_dict["release_date"] = self._get_release_date(game_data)
+        new_dict["developer"] = self._get_developer(game_data)
 
-        age = self._get_required_age(game_data)
-        print(age)
+        self._assign_categories(self._get_categories(game_data), new_dict)
+        self._assign_tags(self._get_genres(game_data), new_dict)
 
-        desc = self._get_description(game_data)
-        print(desc)
-
-        eng = self._is_english(game_data)
-        print(eng)
-
-        win = self._is_windows(game_data)
-        print(win)
-
-        mac = self._is_mac(game_data)
-        print(mac)
-
-        lin = self._is_linux(game_data)
-        print(lin)
-
-        ach = self._get_achievements(game_data)
-        print(ach)
-
-        rd = self._get_release_date(game_data)
-        print(rd)
-
-        catt = self._get_categories(game_data)
-        print(catt)
-
-        genn = self._get_genres(game_data)
-        print(genn)
-
-        dev = self._get_developer(game_data)
-        print(dev)
-
-        pub = self._get_publisher(game_data)
-        print(pub)
-
-
+        return new_dict
+        
--- main.py
+++ main.py
@@ -1,20 +1,174 @@
 import json
 import os
 from APIHandler import APIHandler
+from Cleaner import Cleaner
 
-def beautify_json(data):
-    print(json.dumps(data, indent=4))
+base_dict = {
+    "english" : 0,
+    "windows" : 0,
+    "mac" : 0,
+    "linux" : 0,
+    "achievements" : 0,
+    "average_playtime" : 0,
+    "median_playtime" : 0,
+    "owners" : 0,
+    "price" : 0,
+    "Indie" : 0,
+    "Action" : 0,
+    "Adventure" : 0,
+    "Casual" : 0,
+    "Strategy" : 0,
+    "Simulation" : 0,
+    "RPG" : 0,
+    "Early Access" : 0,
+    "Free to Play" : 0,
+    "Puzzle" : 0,
+    "Racing" : 0,
+    "VR" : 0,
+    "Sports" : 0,
+    "Anime" : 0,
+    "Visual Novel" : 0,
+    "Platformer" : 0,
+    "Point & Click" : 0,
+    "Horror" : 0,
+    "Nudity" : 0,
+    "FPS" : 0,
+    "Multiplayer" : 0,
+    "Sexual Content" : 0,
+    "Violent" : 0,
+    "Gore" : 0,
+    "Massively Multiplayer" : 0,
+    "Hidden Object" : 0,
+    "Open World" : 0,
+    "Survival" : 0,
+    "Pixel Graphics" : 0,
+    "Space" : 0,
+    "Female Protagonist" : 0,
+    "Shoot 'Em Up" : 0,
+    "RTS" : 0,
+    "Classic" : 0,
+    "Turn-Based" : 0,
+    "Arcade" : 0,
+    "Sci-fi" : 0,
+    "Story Rich" : 0,
+    "Tower Defense" : 0,
+    "Zombies" : 0,
+    "Singleplayer" : 0,
+    "World War II" : 0,
+    "Card Game" : 0,
+    "RPGMaker" : 0,
+    "Great Soundtrack" : 0,
+    "Management" : 0,
+    "Co-op" : 0,
+    "Fighting" : 0,
+    "2D" : 0,
+    "Turn-Based Strategy" : 0,
+    "Fantasy" : 0,
+    "Utilities" : 0,
+    "Rogue-like" : 0,
+    "Sandbox" : 0,
+    "JRPG" : 0,
+    "Board Game" : 0,
+    "Retro" : 0,
+    "Stealth" : 0,
+    "Comedy" : 0,
+    "Shooter" : 0,
+    "Music" : 0,
+    "Hack and Slash" : 0,
+    "Bullet Hell" : 0,
+    "Atmospheric" : 0,
+    "First-Person" : 0,
+    "City Builder" : 0,
+    "Historical" : 0,
+    "Psychological Horror" : 0,
+    "Family Friendly" : 0,
+    "Memes" : 0,
+    "Match 3" : 0,
+    "Mystery" : 0,
+    "Difficult" : 0,
+    "Local Multiplayer" : 0,
+    "Driving" : 0,
+    "Design & Illustration" : 0,
+    "Cyberpunk" : 0,
+    "Flight" : 0,
+    "Building" : 0,
+    "Clicker" : 0,
+    "Walking Simulator" : 0,
+    "Metroidvania" : 0,
+    "Education" : 0,
+    "Single-player" : 0,
+    "Steam Achievements" : 0,
+    "Steam Trading Cards" : 0,
+    "Steam Cloud" : 0,
+    "Full controller support" : 0,
+    "Multi-player" : 0,
+    "Partial Controller Support" : 0,
+    "Steam Leaderboards" : 0,
+    "Online Multi-Player" : 0,
+    "Shared/Split Screen" : 0,
+    "Stats" : 0,
+    "Co-op" : 0,
+    "Local Multi-Player" : 0,
+    "Cross-Platform Multiplayer" : 0,
+    "Online Co-op" : 0,
+    "Includes level editor" : 0,
+    "Steam Workshop" : 0,
+    "Local Co-op" : 0,
+    "Captions available" : 0,
+    "In-App Purchases" : 0,
+    "MMO" : 0,
+    "VR Support" : 0,
+    "Commentary available" : 0,
+    "Valve Anti-Cheat enabled" : 0,
+    "Steam Turn Notifications" : 0,
+    "SteamVR Collectibles" : 0,
+    "Includes Source SDK" : 0,
+    "Mods" : 0,
+    "desc" : "a",
+    "name" : "a",
+    "developer" : "a",
+    "release_date" : "a",
+    "appid" : 0,
+    "user_reviews" : 0
+}
 
-appid_list = []
-
-# https://store.steampowered.com/api/appdetails?appids=578080&l=en
+categories_list = ["Single-player", "Steam Achievements", "Steam Trading Cards", "Steam Cloud", "Full controller support", "Multi-player", "Partial Controller Support", "Steam Leaderboards", "Online Multi-Player", "Shared/Split Screen", "Stats", "Co-op", "Local Multi-Player", "Cross-Platform Multiplayer", "Online Co-op", "Includes level editor", "Steam Workshop", "Local Co-op", "Captions available", "In-App Purchases", "MMO", "VR Support", "Commentary available", "Valve Anti-Cheat enabled", "Steam Turn Notifications", "SteamVR Collectibles", "Includes Source SDK", "Mods"]
+tags_list = ["Indie", "Action", "Adventure", "Casual", "Strategy", "Simulation", "RPG", "Early Access", "Free to Play", "Puzzle", "Racing", "VR", "Sports", "Anime", "Visual Novel", "Platformer", "Point & Click", "Horror", "Nudity", "FPS", "Multiplayer", "Sexual Content", "Violent", "Gore", "Massively Multiplayer", "Hidden Object", "Open World", "Survival", "Pixel Graphics", "Space", "Female Protagonist", "Shoot 'Em Up", "RTS", "Classic", "Turn-Based", "Arcade", "Sci-fi", "Story Rich", "Tower Defense", "Zombies", "Singleplayer", "World War II", "Card Game", "RPGMaker", "Great Soundtrack", "Management", "Co-op", "Fighting", "2D", "Turn-Based Strategy", "Fantasy", "Utilities", "Rogue-like", "Sandbox", "JRPG", "Board Game", "Retro", "Stealth", "Comedy", "Shooter", "Music", "Hack and Slash", "Bullet Hell", "Atmospheric", "First-Person", "City Builder", "Historical", "Psychological Horror", "Family Friendly", "Memes", "Match 3", "Mystery", "Difficult", "Local Multiplayer", "Driving", "Design & Illustration", "Cyberpunk", "Flight", "Building", "Clicker", "Walking Simulator", "Metroidvania", "Education"]
 
 base_url = r"https://store.steampowered.com/api/appdetails?appids="
 flags = [r"l=en"]
 
+data_cleaner = Cleaner(base_dict, categories_list, tags_list)
 handler = APIHandler(base_url, flags)
 
-appinfo = handler.fetch_info(578080)
 
-beautify_json(appinfo)
+# # https://store.steampowered.com/api/appdetails?appids=578080&l=en
+
+appids = [[1150690, None], [274520, None], [1245620, None], [1142710, None], [445870, None], [1171120, None]]
+
+for count, appid in enumerate(appids, 0):
+    appinfo = handler.fetch_steam_info(appid[0])
+    cleaned_data = data_cleaner.clean_data(appinfo, str(appid[0]))
+
+    cleaned_data["appid"] = appid[0]
+
+    playtime_data = handler.fetch_howlongtobeat_info(cleaned_data["name"])
+
+    if playtime_data == None:
+        cleaned_data["average_playtime"] = 20
+        cleaned_data["median_playtime"] = 20
+    else: 
+        playtime_hours = playtime_data.gameplay_main
+
+        if "½" in playtime_hours:
+            playtime_hours = playtime_hours[:-1]
+
+        playtime_hours = int(playtime_hours)
+
+        cleaned_data["average_playtime"] = playtime_hours
+        cleaned_data["median_playtime"] = playtime_hours
+
+    appids[count][1] = cleaned_data
+
+
 
